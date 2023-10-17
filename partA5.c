549c8ee797fc560ab4740a07b1dfc7b1db2431e3
KUMAR SATYAM <90494688+kumarcodes3004@users.noreply.github.com>
2023-10-17 15:33:59 +0530

    Update partA5.c

--- partA5.c
+++ partA5.c
@@ -1,31 +1,43 @@
-#include<stdio.h>
-#include<omp.h>
-#include<stdlib.h>
-int isprime(int n){
-    // #pragma omp critical
-    if(n<=1) return 0;
-    for(int i=2;i<n;i++){
-        if(n%i==0) return 0;
+#include <stdio.h>
+#include <omp.h>
+#include <time.h>
+#include <stdlib.h>
+int isPrime(int n)
+{
+    if (n <= 1)
+        return 0;
+#pragma omp parallel for
+    for (int i = 2; i < n; i++)
+    {
+        if (n % i == 0)
+            return 0;
     }
     return 1;
 }
-void main(){
-    int n=50;
-    int *prime=(int*)calloc(n,sizeof(int));
-    double s=omp_get_wtime();
-    #pragma omp parallel num_threads(16)
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    clock_t s1, e1, s2, e2;
+    s1 = clock();
+#pragma omp parallel for
+    for (int i = 0; i < n; i++)
     {
-       #pragma omp for
-       for(int i=0;i<n;i++){
-       if(isprime(i)){
-         prime[i]=1;
-       }
-      }
+        if (isPrime(i))
+            continue;
+        // printf("%d\t",i);
     }
-    double e=omp_get_wtime();
-    for(int i=0;i<n;i++){
-        if(prime[i]) printf("%d ",i);
+    printf("\n");
+    e1 = clock();
+    printf("Time for Parallel Execution: %lf\n", (double)(e1 - s1) / CLOCKS_PER_SEC);
+    s2 = clock();
+    for (int i = 0; i < n; i++)
+    {
+        if (isPrime(i))
+            continue;
+        // printf("%d\t",i);
     }
-    printf("\nTime: %lfs",e-s);
-    
+    e2 = clock();
+    printf("\n");
+    printf("Time for Serial Execution: %lf\n", (double)(e2 - s2) / CLOCKS_PER_SEC);
 }
